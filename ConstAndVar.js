6ba7f4f2e21b673701fbcda260e1e1c753cf4bef
Muhammad Hussnain <muhammadhusnain5020@gmail.com>
2024-09-19 13:06:42 +0500

    learn about the var and the const

---
+++ ConstAndVar.js
@@ -0,0 +1,12 @@
+const accountId = 12345;
+let email = "hameesh@gmail.com";
+var pass = "2222"
+accountLoan = 2
+
+email = "hameesha@gmail.com";
+pass = "1111";
+accountLoan = 3
+
+console.table([accountId , accountLoan , email , pass])
+// accountId = 5; // const cannot be change after it commited once because it's constant 
+console.log(accountId);
